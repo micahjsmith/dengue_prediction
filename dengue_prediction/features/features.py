9872a280c79a77e263cdaf0c9a5a35940c888f71
Micah Smith <micahjsmith@gmail.com>
2018-03-30 19:33:21 -0400

    Implement more features using exciting transformers

--- dengue_prediction/features/features.py
+++ dengue_prediction/features/features.py
@@ -1,6 +1,9 @@
+import numpy as np
+import pandas as pd
 import sklearn.preprocessing
 
 from dengue_prediction.features.feature import Feature
+from dengue_prediction.features.transformers import NullFiller, SimpleFunctionTransformer, NullIndicator, SingleLagger, LagImputer
 
 
 def get_features():
@@ -45,5 +48,36 @@ def get_features():
             ]
         )
     )
+
+    features.append(
+        Feature(
+            input='precipitation_amt_mm',
+            transformer=[
+                sklearn.preprocessing.Imputer(),
+                SimpleFunctionTransformer(np.log1p)
+            ]
+        )
+    )
+    
+    features.append(
+        Feature(
+            input='precipitation_amt_mm',
+            transformer=[
+                NullIndicator(),
+            ]
+        )
+    )
+    
+    features.append(
+        Feature(
+            input='reanalysis_sat_precip_amt_mm',
+            transformer=[
+                SingleLagger(1, groupby_kwargs={'level': 'city'}),
+                LagImputer(groupby_kwargs={'level': 'city'}),
+                NullFiller(replacement=0.0),
+            ]
+        )
+    )
+
 
     return features
