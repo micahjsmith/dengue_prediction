6847d1aa0a726c0f6f4f0195998697e06a6d94d0
Micah Smith <micahjsmith@gmail.com>
2018-03-30 13:01:06 -0400

    Make robust transformer pipeline

--- dengue_prediction/features/feature.py
+++ dengue_prediction/features/feature.py
@@ -4,40 +4,66 @@ import logging
 import funcy
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import _name_estimators
+from sklearn_pandas.pipeline import TransformerPipeline
+
+
+from dengue_prediction.util import asarray2d
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
-def make_approaches(funcs, catch):
+__all__ = ['Feature', 'FeatureValidator']
+
+
+class RobustTransformerPipeline(TransformerPipeline):
+    
+    def transform(self, X, *args, **kwargs):
+        _transform = _make_robust_to_tabular_types(
+            super().transform, 1)
+        return _transform(X, *args, **kwargs)
+    
+    
+def make_robust_transformer_pipeline(*steps):
+    """Construct a TransformerPipeline from the given estimators.
+    """
+    return RobustTransformerPipeline(_name_estimators(steps))
+
+
+def _make_approaches(funcs, catch):
     if funcs:
         for func in funcs[:-1]:
             yield func, catch 
         yield funcs[-1], ()
         
 
-def _make_robust_to_tabular_type(func, n=2):
+def _make_robust_to_tabular_types(func, n):
     @funcy.wraps(func)
     def wrapped(*args, **kwargs):
-        approaches = make_approaches(
-            (funcy.identity, pd.DataFrame, np.asarray),
+        approaches = _make_approaches(
+            (funcy.identity,
+             pd.DataFrame,
+             np.asarray,
+             asarray2d),
             (ValueError, TypeError) 
         )
+        nonconverted_args = args[n:]
         for convert, catch in approaches:
             try:
                 converted_args = [convert(args[j]) for j in range(n)]
-                original_args = args[n:]
+                args_ = funcy.merge(converted_args, nonconverted_args)
                 logger.debug("Converting using approach '{}'".format(convert.__name__))
-                return func(*converted_args, *original_args, **kwargs)
+                return func(*args_, **kwargs)
             except catch:
                 pass
     return wrapped
             
         
 def make_robust(transformer):
-    transformer.fit = _make_robust_to_tabular_type(transformer.fit, n=2)
-    transformer.transform = _make_robust_to_tabular_type(transformer.transform, n=1)
+    transformer.fit = _make_robust_to_tabular_types(transformer.fit, 2)
+    # todo optionally catch all errors of transformer and return []
+    transformer.transform = _make_robust_to_tabular_types(transformer.transform, 1)
     return transformer 
 
     
@@ -46,11 +72,15 @@ class Feature:
                  name=None, description=None, output=None,
                  options = {}):
         self.input = input
-        self.transformer = make_robust(transformer)
         self.name = name
         self.description = description
         self.output = output
         self.options = options
+        
+        if funcy.is_seqcont(transformer):
+            transformer = make_robust_transformer_pipeline(*transformer)
+        self.transformer = make_robust(transformer)
+        
         
     def as_sklearn_pandas_tuple(self):
         return (self.input, self.transformer)
