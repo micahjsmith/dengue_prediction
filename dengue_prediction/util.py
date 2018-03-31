b8f9f4220d10949a09a18f98b856ba115c925927
Micah Smith <micahjsmith@gmail.com>
2018-03-31 16:45:58 -0400

    Implement InputLogger and LoggingContext utils

--- dengue_prediction/util.py
+++ dengue_prediction/util.py
@@ -1,3 +1,5 @@
+import inspect
+import logging
 import random
 
 import funcy
@@ -67,6 +69,82 @@ class FragileTransformerPipeline(TransformerPipeline):
         super().__init__(steps)
 
 
+def get_arr_desc(arr):
+    desc = '{typ} {shp}'
+    typ = type(arr)
+    shp = getattr(arr, 'shape', None)
+    return desc.format(typ=typ, shp=shp)
+
+
+class InputLogger(BaseEstimator, TransformerMixin):
+    def __init__(self, name=None, level='debug'):
+        initialized = False
+        if isinstance(level, int):
+            self.level = level
+            initialized = True
+        elif isinstance(level, str):
+            level = level.upper()
+            if hasattr(logging, level):
+                self.level = getattr(logging, level)
+                initialized = True
+
+        if not initialized:
+            raise ValueError('Invalid level: {}'.format(level))
+
+        self.name = name
+
+    def _log(self, msg):
+        # Extract current *.py file name
+        # Source: https://stackoverflow.com/a/28645157/2514228
+        # TODO get a couple frames up in the stack to get accurate logger name
+        if self.name:
+            name = self.name
+        else:
+            name = (inspect.getfile(inspect.currentframe()).split("\\", -1)[-1]).rsplit(".", 1)[0]
+        return logging.getLogger(name).log(self.level, msg)
+
+    def fit(self, X, y=None, **fit_kwargs):
+        X_desc = get_arr_desc(X)
+        y_desc = get_arr_desc(y)
+        self._log('Fit called with X={X_desc}, y={y_desc}'.format(X_desc=X_desc, y_desc=y_desc))
+        return self
+
+    def transform(self, X, **transform_kwargs):
+        X_desc = get_arr_desc(X)
+        self._log('Transform called with X={X_desc}'.format(X_desc=X_desc))
+        return X
+
+
 def indent(text, n=4):
     _indent = ' '*n
     return '\n'.join([_indent + line for line in text.split('\n')])
+
+
+class LoggingContext(object):
+    '''
+    Logging context manager
+
+    Source: <https://docs.python.org/3/howto/logging-cookbook.html#using-a-context-manager-for-selective-logging>
+    '''
+
+    def __init__(self, logger, level=None, handler=None, close=True):
+        self.logger = logger
+        self.level = level
+        self.handler = handler
+        self.close = close
+
+    def __enter__(self):
+        if self.level is not None:
+            self.old_level = self.logger.level
+            self.logger.setLevel(self.level)
+        if self.handler:
+            self.logger.addHandler(self.handler)
+
+    def __exit__(self, et, ev, tb):
+        if self.level is not None:
+            self.logger.setLevel(self.old_level)
+        if self.handler:
+            self.logger.removeHandler(self.handler)
+        if self.handler and self.close:
+            self.handler.close()
+        # implicit return of None => don't swallow exceptions
