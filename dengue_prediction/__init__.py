add610fe48c8d079a698b7f57cacc0b434527d56
Micah Smith <micahjsmith@gmail.com>
2018-04-02 11:37:16 -0400

    Filter h5py warning

--- dengue_prediction/__init__.py
+++ dengue_prediction/__init__.py
@@ -1,7 +1,15 @@
 import pathlib
+import warnings
 
 _PROJECT_ROOT = pathlib.Path(__file__).parent.parent
 PROJECT_PATHS = {}
 PROJECT_PATHS['root'] = _PROJECT_ROOT
 PROJECT_PATHS['data'] = _PROJECT_ROOT / 'data'
 PROJECT_PATHS['rawdata'] = _PROJECT_ROOT / 'data' / 'raw'
+
+warnings.filterwarnings(
+    'ignore',
+    message='Conversion of the second argument of issubdtype',
+    module='h5py',
+    lineno=36,
+)
