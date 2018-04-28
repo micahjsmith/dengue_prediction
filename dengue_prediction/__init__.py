ebeafb3e66e24e58194d6a92be6b29a361b36997
Micah Smith <micahjsmith@gmail.com>
2018-04-28 16:46:03 -0400

    Expose PROJECT_ROOT

--- dengue_prediction/__init__.py
+++ dengue_prediction/__init__.py
@@ -1,11 +1,11 @@
 import pathlib
 import warnings
 
-_PROJECT_ROOT = pathlib.Path(__file__).parent.parent
+PROJECT_ROOT = pathlib.Path(__file__).parent.parent
 PROJECT_PATHS = {}
-PROJECT_PATHS['root'] = _PROJECT_ROOT
-PROJECT_PATHS['data'] = _PROJECT_ROOT / 'data'
-PROJECT_PATHS['rawdata'] = _PROJECT_ROOT / 'data' / 'raw'
+PROJECT_PATHS['root'] = PROJECT_ROOT
+PROJECT_PATHS['data'] = PROJECT_ROOT / 'data'
+PROJECT_PATHS['rawdata'] = PROJECT_ROOT / 'data' / 'raw'
 
 warnings.filterwarnings(
     'ignore',
