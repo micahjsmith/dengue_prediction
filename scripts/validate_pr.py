6ce0bdc29019992a2c681fcfaf2f00a806e05e83
Micah Smith <micahjsmith@gmail.com>
2018-04-28 21:43:54 -0400

    Update logging in validate_pr

--- scripts/validate_pr.py
+++ scripts/validate_pr.py
@@ -14,9 +14,10 @@ logger = logging.getLogger(__name__)
 @click.command()
 @click.argument('pr_num', required=False, default=None, type=int)
 def main(pr_num=None):
-    RETVAL_NOT_PR = -1
+
     RETVAL_VALID = 0
-    RETVAL_INVALID = 1
+    RETVAL_NOT_PR = 1
+    RETVAL_INVALID = 2
 
     if pr_num is None:
         logger.info('No PR provided. Trying to detect PR from Travis ENV.')
@@ -28,14 +29,19 @@ def main(pr_num=None):
             logger.info('Detected PR {}'.format(pr_num))
 
     logger.info('Validating PR {}...'.format(pr_num))
+
     validator = PullRequestFeatureValidator(pr_num)
     result = validator.validate()
+
     if result is True:
         return RETVAL_VALID
     else:
         return RETVAL_INVALID
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('fhub_core').setLevel(logging.CRITICAL)
+    _logger = logging.getLogger('dengue_prediction')
+    _logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    _logger.addHandler(handler)
     sys.exit(main())
